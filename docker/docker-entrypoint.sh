1e3df75d75e9f3ee1f4216180f99386ef14e298b
shuye <80120075+wuzhi01@users.noreply.github.com>
2021-03-09 14:16:01 +0800

    update

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -59,7 +59,7 @@ if [[ ${ENABLE_WEB_PANEL} == true ]]; then
   cd ${JD_DIR}/panel
   pm2 start ${JD_DIR}/panel/server.js --watch "${JD_DIR}/panel/server.js" --name="server"
   echo -e "控制面板启动成功...\n"
-  echo -e "如未修改用户名密码，则初始用户名为：admin，初始密码为：adminadmin\n"
+  echo -e "如未修改用户名密码，则初始用户名为：admin，初始密码为：shuye72\n"
   echo -e "请访问 http://<ip>:5678 登陆并修改配置...\n"
 elif [[ ${ENABLE_WEB_PANEL} == false ]]; then
   echo -e "已设置为不自动启动控制面板，跳过...\n"
--- git_pull.sh
+++ git_pull.sh
@@ -23,7 +23,7 @@ ContentNewTask=${ShellDir}/new_task
 ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
 isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
-ShellURL=https://hub.fastgit.org/shuyeshuye/TEST
+ShellURL=https://hub.fastgit.org/shuyeshuye/jd-base
 ScriptsURL=https://gitee.com/shuye72/MyActions
 
 ## 更新crontab，gitee服务器同一时间限制5个链接，因此每个人更新代码必须错开时间，每次执行git_pull随机生成。
